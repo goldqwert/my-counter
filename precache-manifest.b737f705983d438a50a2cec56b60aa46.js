6dab55452d0c7fe9b84e04bb38fc8df1a561686f
Yury <goldyukol12@gmail.com>
2019-11-03 23:50:40 +0300

    Updates

---
+++ precache-manifest.b737f705983d438a50a2cec56b60aa46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b4ef6905df4e7782b4398afa537f50",
+    "url": "/MyCounter/index.html"
+  },
+  {
+    "revision": "eb3621d10196049cba9e",
+    "url": "/MyCounter/static/css/main.bcaae821.chunk.css"
+  },
+  {
+    "revision": "0e7ed7a8638174a43c32",
+    "url": "/MyCounter/static/js/2.7a9ca136.chunk.js"
+  },
+  {
+    "revision": "eb3621d10196049cba9e",
+    "url": "/MyCounter/static/js/main.cebac318.chunk.js"
+  },
+  {
+    "revision": "f9fdccf26a946d0ebdfa",
+    "url": "/MyCounter/static/js/runtime-main.a3872da7.js"
+  },
+  {
+    "revision": "62edef52ea54b4def6f9c484b590042c",
+    "url": "/MyCounter/static/media/css.62edef52.jpg"
+  }
+]);
